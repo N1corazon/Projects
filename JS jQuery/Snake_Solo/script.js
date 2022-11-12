a69a7fd2edc4f228f52eeaa1d0b10d3ab1da7620
Nicorazon <nikosstatski@outlook.com.gr>
2022-11-12 23:52:06 +0200

    Added: Player Control Snake Finished CSS, HTML

--- JS jQuery/Snake_Solo/script.js
+++ JS jQuery/Snake_Solo/script.js
@@ -1,6 +1,10 @@
-$(function (){
+$(function () {
     let gameArea = $("#game-area");
     let maxGameSize = 20;
+    let currentPlayerDir = 1;
+    let playerLength = 4;
+    let frameCount = 0;
+
 
     let playerPos = {
         "tr": 9,
@@ -14,21 +18,70 @@ $(function (){
         "left": 4
     }
 
+    document.addEventListener("keydown", function (event) {
 
+        if (event.keyCode == '38') {
+            currentPlayerDir = playerDir["up"];
+        }
+        else if (event.keyCode == '40') {
+            currentPlayerDir = playerDir["down"];
+        }
+        else if (event.keyCode == '37') {
+            currentPlayerDir = playerDir["left"];
+        }
+        else if (event.keyCode == '39') {
+            if
+            currentPlayerDir = playerDir["right"];
+        }
+    })
 
-    function drawGame(){
-        for (let index = 0; index < maxGameSize; index++){
-            gameArea.append("<tr class='tr" + index + "'></tr>" );
+    function drawGame() {
+        for (let index = 0; index < maxGameSize; index++) {
+            gameArea.append("<tr class='tr" + index + "'></tr>");
 
             let thisTr = $(".tr" + index);
-            for (let indexTd = 0; indexTd < maxGameSize; indexTd++){
+            for (let indexTd = 0; indexTd < maxGameSize; indexTd++) {
                 thisTr.append("<td class='tr" + index + "td" + indexTd + "'></td>");
             }
         }
     }
+
     drawGame();
 
-    function drawPlayer(){
+    function drawPlayer() {
+        frameCount++;
 
+        let drawPlayer = $(".tr" + playerPos["tr"] + "td" + playerPos["td"]);
+        drawPlayer.addClass("draw-player frameCount" + frameCount);
+
+        switch (currentPlayerDir) {
+            case 1:
+                playerPos["tr"]--;
+                break;
+            case 2:
+                playerPos["td"]++;
+
+                break;
+            case 3:
+                playerPos["tr"]++;
+
+                break;
+            case 4:
+                playerPos["td"]--;
+
+                break;
+            default :
+
+        }
+
+        let removeTail = frameCount - playerLength;
+        let removeTailClass = $(".frameCount" + removeTail);
+        removeTailClass.removeClass("draw-player frameCount" + removeTail);
     }
+
+    setInterval(function () {
+        drawPlayer();
+
+    }, 200);
+
 });
